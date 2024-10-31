7ec84c70953a73c54678139b21b2fdbfcec27389
Mike Lei <mikelei8291@users.noreply.github.com>
2024-11-01 04:14:06 +0800

    feat(main): add Redis database connection to handler dependencies

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ mod subscription;
 #[tokio::main]
 async fn main() -> Result<(), RequestError> {
     let bot = Bot::from_env();
+    let client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let db = client.get_multiplexed_async_connection().await.unwrap();
     bot.set_my_commands(Command::bot_commands()).await.expect("Loading bot commands failed.");
     let handler = dptree::entry().branch(
         Update::filter_message().branch(
@@ -23,6 +25,7 @@ async fn main() -> Result<(), RequestError> {
         Update::filter_callback_query().endpoint(callback_handler)
     );
     Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![db])
         .default_handler(|update| async move { warn!("Unhandled update: {update:?}") })
         .error_handler(LoggingErrorHandler::with_custom_text("Dispatcher error"))
         .enable_ctrlc_handler()
