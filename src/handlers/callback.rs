28937566555f3d97d261d5a6e22727ff6be47269
Mike Lei <mikelei8291@users.noreply.github.com>
2024-11-01 04:20:05 +0800

    feat(handler): implement callback handler for the "sub" callback

--- src/handlers/callback.rs
+++ src/handlers/callback.rs
@@ -1,22 +1,47 @@
-use teloxide::{prelude::Requester, types::CallbackQuery, Bot, RequestError};
+use std::borrow::BorrowMut;
 
-pub async fn callback_handler(bot: Bot, query: CallbackQuery) -> Result<(), RequestError> {
+use redis::{aio::MultiplexedConnection, AsyncCommands, RedisError};
+use teloxide::{payloads::AnswerCallbackQuerySetters, prelude::Requester, types::CallbackQuery, Bot, RequestError};
+
+use crate::subscription::Subscription;
+
+pub async fn callback_handler(bot: Bot, query: CallbackQuery, mut db: MultiplexedConnection) -> Result<(), RequestError> {
     if let Some(ref data) = query.data {
-        bot.answer_callback_query(&query.id).await?;
-        let text = match data.as_str() {
-            "sub" => {
-                // Do subscribe
-                "You have successfully subscribed to {} user: {}"
-            }
-            "del" => {
-                // Do unsubscribe
-                "You have successfully unsubscribed to {} user: {}"
-            }
-            "cancel" => "Cancelled",
-            _ => "Why are we still here? Just to suffer?"
+        let Some(msg) = query.regular_message() else {
+            bot.answer_callback_query(&query.id).text("Message expired, please use the command again").await?;
+            return Ok(());
         };
-        if let Some(msg) = query.regular_message() {
+        if data == "cancel" { // handle cancel callback first
+            bot.edit_message_text(msg.chat.id, msg.id, "Cancelled").await?;
+            return Ok(());
+        }
+        let key = format!("{}-{}", msg.chat.id, msg.id);
+        let Ok(sub_str): Result<Option<String>, RedisError> = db.get(&key).await else {
+            bot.answer_callback_query(&query.id).text("Database error").await?;
+            return Ok(());
+        };
+        if let Some(sub_str) = sub_str {
+            let Ok(sub) = sub_str.parse::<Subscription>() else {
+                panic!("Wrong, too wrong");
+            };
+            let mut pipe = redis::pipe().atomic().to_owned();
+            let (text, pipe) = match data.as_str() {
+                "sub" => {
+                    let pipe = pipe
+                        .sadd(format!("sub:{sub_str}"), &query.from.id.to_string())
+                        .sadd(&query.from.id.to_string(), &sub_str);
+                    (format!("You have successfully subscribed to {} user: {}", sub.platform, sub.user_id), pipe)
+                }
+                "del" => {
+                    // Do unsubscribe
+                    (format!("You have successfully unsubscribed to {} user: {}", sub.platform, sub.user_id), pipe.borrow_mut())
+                }
+                _ => ("Why are we still here? Just to suffer?".to_owned(), pipe.borrow_mut())
+            };
+            pipe.del(key).exec_async(&mut db).await.unwrap();
             bot.edit_message_text(msg.chat.id, msg.id, text).await?;
+        } else {
+            bot.answer_callback_query(&query.id).text("Message expired, please use the command again").await?;
         }
     }
     Ok(())
