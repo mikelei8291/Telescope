ab946a59737eb228d88790055cecb0e82dee651d
Mike Lei <mikelei8291@users.noreply.github.com>
2024-11-02 14:51:04 +0800

    fix(handler): add help message to /list command when user has no subs

--- src/handlers/command.rs
+++ src/handlers/command.rs
@@ -85,7 +85,7 @@ pub async fn command_handler(bot: Bot, msg: Message, cmd: Command, mut db: Multi
             if let Ok(results) = db.sscan::<_, String>(msg.chat.id.to_string()).await {
                 let subs = results.enumerate().map(|(i, r)| format!("{}\\. {r}", i + 1)).collect::<Vec<String>>().await.join("\n");
                 if subs == "".to_owned() {
-                    bot.send_message(msg.chat.id, "You have no subscriptions").await?
+                    bot.send_message(msg.chat.id, "You have no subscriptions\\.\nUse the /sub command to add new subscriptions.").await?
                 } else {
                     bot.send_message(msg.chat.id, format!("Your subscriptions:\n{subs}")).await?
                 }
