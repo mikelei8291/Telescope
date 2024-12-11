bf26fcb2e13147fbeffc2de6f27f63c47d96776b
Mike Lei <mikelei8291@users.noreply.github.com>
2024-12-11 23:15:34 +0800

    feat(handlers): produce only one confirmation message for multiple URLs

--- src/handlers/callback.rs
+++ src/handlers/callback.rs
@@ -1,17 +1,28 @@
-use log::error;
+use std::num::NonZero;
+
 use redis::{aio::MultiplexedConnection, AsyncCommands};
 use teloxide::{
-    payloads::AnswerCallbackQuerySetters, prelude::Requester, types::CallbackQuery, utils::markdown::escape,
-    RequestError,
+    payloads::AnswerCallbackQuerySetters, prelude::Requester, types::{CallbackQuery, Message}, RequestError,
 };
 
-use crate::{subscription::Subscription, Bot};
+use crate::{subscription::fmt_subscriptions, Bot};
+
+use super::command::Action;
+
+const EXPIRED_MESSAGE: &str = "Message expired, please use the command again";
+
+async fn error_callback_query(bot: &Bot, query: &CallbackQuery, msg: Option<&Message>) -> Result<(), RequestError> {
+    bot.answer_callback_query(&query.id).text(EXPIRED_MESSAGE).await?;
+    if let Some(msg) = msg {
+        bot.edit_message_text(msg.chat.id, msg.id, EXPIRED_MESSAGE).await?;
+    }
+    Ok(())
+}
 
 pub async fn callback_handler(bot: Bot, query: CallbackQuery, mut db: MultiplexedConnection) -> Result<(), RequestError> {
     if let Some(ref data) = query.data {
         let Some(msg) = query.regular_message() else {
-            bot.answer_callback_query(&query.id).text("Message expired, please use the command again").await?;
-            return Ok(());
+            return error_callback_query(&bot, &query, None).await;
         };
         let key = format!("{}:{}", msg.chat.id, msg.id);
         if data == "cancel" { // handle cancel callback first
@@ -19,50 +30,37 @@ pub async fn callback_handler(bot: Bot, query: CallbackQuery, mut db: Multiplexe
             let _: () = db.del(key).await.unwrap();
             return Ok(());
         }
-        let Ok(sub_str): Result<Option<String>, _> = db.get(&key).await else {
-            bot.answer_callback_query(&query.id).text("Database error").await?;
-            return Ok(());
+        let len = db.llen(&key).await.ok().map(|l| NonZero::new(l).unwrap());
+        let Ok(subs) = db.lpop(&key, len).await else {
+            return error_callback_query(&bot, &query, Some(msg)).await;
         };
-        if let Some(sub_str) = sub_str {
-            let Ok(sub) = sub_str.parse::<Subscription>() else {
-                bot.answer_callback_query(&query.id).text("Database error").await?;
-                error!("Wrong, too wrong");
-                return Ok(());
-            };
-            let mut pipe = redis::pipe();
-            let pipe = pipe.atomic();
-            let (text, pipe) = match data.as_str() {
-                "sub" => {
-                    let pipe = pipe
-                        .hset("subs", &sub_str, "")
-                        .hset(&sub_str, &query.from.id.to_string(), 0)
-                        .sadd(&query.from.id.to_string(), &sub_str);
-                    (format!(
-                        "You have successfully subscribed to *{}* user: *{}*",
-                        sub.platform,
-                        escape(sub.user.username.as_str())
-                    ), pipe)
+        let mut pipe = redis::pipe();
+        let mut pipe = pipe.atomic();
+        let (text, pipe) = match data.parse() {
+            Ok(Action::Subscribe) => {
+                for sub in &subs {
+                    pipe = pipe
+                        .hset("subs", sub, "")
+                        .hset(sub, &query.from.id.to_string(), 0)
+                        .sadd(&query.from.id.to_string(), sub);
                 }
-                "del" => {
-                    let mut pipe = pipe
-                        .srem(&query.from.id.to_string(), &sub_str)
-                        .hdel(&sub_str, &query.from.id.to_string());
-                    if db.hlen::<_, u64>(&sub_str).await.unwrap() == 1 {
-                        pipe = pipe.hdel("subs", &sub_str)
+                (format!("You have successfully subscribed to:\n{}", fmt_subscriptions(&subs)), pipe)
+            }
+            Ok(Action::Unsubscribe) => {
+                for sub in &subs {
+                    pipe = pipe
+                        .srem(&query.from.id.to_string(), sub)
+                        .hdel(sub, &query.from.id.to_string());
+                    if db.hlen::<_, u64>(sub).await.unwrap() == 1 {
+                        pipe = pipe.hdel("subs", sub)
                     }
-                    (format!(
-                        "You have successfully unsubscribed to *{}* user: *{}*",
-                        sub.platform,
-                        escape(sub.user.username.as_str())
-                    ), pipe)
                 }
-                _ => ("Why are we still here? Just to suffer?".to_owned(), pipe)
-            };
-            pipe.del(key).exec_async(&mut db).await.unwrap();
-            bot.edit_message_text(msg.chat.id, msg.id, text).await?;
-        } else {
-            bot.answer_callback_query(&query.id).text("Message expired, please use the command again").await?;
-        }
+                (format!("You have successfully unsubscribed to:\n{}", fmt_subscriptions(&subs)), pipe)
+            }
+            Err(_) => ("Why are we still here? Just to suffer?".to_owned(), pipe)
+        };
+        pipe.del(key).exec_async(&mut db).await.unwrap();
+        bot.edit_message_text(msg.chat.id, msg.id, text).await?;
     }
     Ok(())
 }
--- src/handlers/command.rs
+++ src/handlers/command.rs
@@ -1,15 +1,15 @@
 use redis::{aio::MultiplexedConnection, AsyncCommands};
 use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumString};
 use teloxide::{
     payloads::SendMessageSetters,
     prelude::Requester,
-    respond,
     types::{ChatId, InlineKeyboardButton, InlineKeyboardMarkup, Message},
     utils::{command::BotCommands, markdown::escape},
     RequestError
 };
 
-use crate::{platform::Platform, subscription::Subscription, Bot};
+use crate::{platform::Platform, subscription::{fmt_subscriptions, Subscription}, Bot};
 
 #[derive(BotCommands, Clone)]
 #[command(rename_rule = "lowercase")]
@@ -30,10 +30,17 @@ pub enum Command {
     Platform
 }
 
-fn make_reply_markup(action: &str) -> InlineKeyboardMarkup {
+#[derive(Display, EnumString)]
+#[strum(serialize_all = "lowercase")]
+pub enum Action {
+    Subscribe,
+    Unsubscribe
+}
+
+fn make_reply_markup(action: Action) -> InlineKeyboardMarkup {
     let keyboard: [[InlineKeyboardButton; 2]; 1] = [
         [
-            InlineKeyboardButton::callback("✅ Confirm", action.to_owned()),
+            InlineKeyboardButton::callback("✅ Confirm", action.to_string()),
             InlineKeyboardButton::callback("❌ Cancel", "cancel".to_owned())
         ]
     ];
@@ -41,35 +48,36 @@ fn make_reply_markup(action: &str) -> InlineKeyboardMarkup {
 }
 
 async fn send_reply(
-    bot: &Bot, chat_id: ChatId, sub: Subscription, db: &mut MultiplexedConnection, text: &str, action: &str
+    bot: &Bot, chat_id: ChatId, db: &mut MultiplexedConnection, subs: Vec<Subscription>, action: Action
 ) -> Result<Message, RequestError> {
+    let subs_list = fmt_subscriptions(&subs);
     let reply = bot.send_message(
         chat_id,
-        format!("Please confirm that you want to {text} to *{}* user: *{}*", sub.platform, escape(sub.user.username.as_str()))
+        format!("Please confirm that you want to {action} to the following users:\n{subs_list}")
     ).reply_markup(make_reply_markup(action)).await?;
     let key = format!("{}:{}", reply.chat.id, reply.id);
-    redis::pipe().atomic().set(&key, sub.to_db_string()).expire(&key, 86400).exec_async(db).await.unwrap();
+    redis::pipe().atomic().rpush(&key, subs).expire(&key, 86400).exec_async(db).await.unwrap();
     Ok(reply)
 }
 
-async fn parse_urls(bot: &Bot, msg: &Message, cmd: &Command, db: &mut MultiplexedConnection, urls: &String) -> Vec<Subscription> {
+async fn process_urls(
+    bot: &Bot, msg: &Message, db: &mut MultiplexedConnection, urls: &String, action: Action
+) -> Result<Message, RequestError> {
     let mut subs = vec![];
     let mut errors = vec![];
     for url in urls.split(" ") {
         match Subscription::from_url(url.to_owned()).await {
             Ok(sub) => {
-                if let Ok(result) = db.sismember(msg.chat.id.to_string(), sub.to_db_string()).await {
+                if let Ok(result) = db.sismember(msg.chat.id.to_string(), &sub).await {
                     if result {
-                        match cmd {
-                            Command::Sub(_) => errors.push(format!("{}: You have already subscribed to {sub}", escape(url))),
-                            Command::Del(_) => subs.push(sub),
-                            _ => ()
+                        match action {
+                            Action::Subscribe => errors.push(format!("{}: You have already subscribed to {sub}", escape(url))),
+                            Action::Unsubscribe => subs.push(sub),
                         }
                     } else {
-                        match cmd {
-                            Command::Sub(_) => subs.push(sub),
-                            Command::Del(_) => errors.push(format!("{}: You are not subscribed to {sub}", escape(url))),
-                            _ => ()
+                        match action {
+                            Action::Subscribe => subs.push(sub),
+                            Action::Unsubscribe => errors.push(format!("{}: You are not subscribed to {sub}", escape(url))),
                         }
                     }
                 }
@@ -80,7 +88,7 @@ async fn parse_urls(bot: &Bot, msg: &Message, cmd: &Command, db: &mut Multiplexe
     if errors.len() != 0 {
         bot.send_message(msg.chat.id, errors.join("\n")).await.unwrap();
     }
-    subs
+    send_reply(bot, msg.chat.id, db, subs, action).await
 }
 
 pub async fn command_handler(bot: Bot, msg: Message, cmd: Command, mut db: MultiplexedConnection) -> Result<(), RequestError> {
@@ -89,18 +97,8 @@ pub async fn command_handler(bot: Bot, msg: Message, cmd: Command, mut db: Multi
             msg.chat.id, "Welcome to the Telescope bot\\. You can view a list of available commands using the /help command\\."
         ).await?,
         Command::Help => bot.send_message(msg.chat.id, escape(Command::descriptions().to_string().as_str())).await?,
-        Command::Sub(ref urls) => {
-            for sub in parse_urls(&bot, &msg, &cmd, &mut db, urls).await {
-                send_reply(&bot, msg.chat.id, sub, &mut db, "subscribe", "sub").await?;
-            }
-            return respond(());
-        }
-        Command::Del(ref urls) => {
-            for sub in parse_urls(&bot, &msg, &cmd, &mut db, urls).await {
-                send_reply(&bot, msg.chat.id, sub, &mut db, "unsubscribe", "del").await?;
-            }
-            return respond(());
-        }
+        Command::Sub(ref urls) => process_urls(&bot, &msg, &mut db, urls, Action::Subscribe).await?,
+        Command::Del(ref urls) => process_urls(&bot, &msg, &mut db, urls, Action::Unsubscribe).await?,
         Command::List => {
             if let Ok(results) = db.smembers::<_, Vec<String>>(msg.chat.id.to_string()).await {
                 let subs = results.iter().enumerate().filter_map(|(i, r)| {
@@ -123,5 +121,5 @@ pub async fn command_handler(bot: Bot, msg: Message, cmd: Command, mut db: Multi
             bot.send_message(msg.chat.id, format!("Supported platforms:\n{platforms}")).await?
         }
     };
-    respond(())
+    Ok(())
 }
