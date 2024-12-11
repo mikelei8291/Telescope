5c121ea3784a4d983ead1d64e16276dd640e888f
Mike Lei <mikelei8291@users.noreply.github.com>
2024-12-11 23:17:27 +0800

    refactor(watcher): use redis traits for `Subscription` objects

--- src/watcher.rs
+++ src/watcher.rs
@@ -9,18 +9,14 @@ use crate::{apis::{get_bilibili_api, get_twitter_api, LiveState, Metadata, API},
 pub async fn check<T: Metadata + Display>(api: Arc<impl API<T>>, db: &mut MultiplexedConnection, bot: &Bot, platform: Platform) {
     let mut subs: Vec<Subscription> = vec![];
     let mut db_clone = db.clone();
-    let mut iter: AsyncIter<(String, String)> = db_clone.hscan_match("subs", format!("{}:*", platform)).await.unwrap();
-    while let Some((sub_str, live_id)) = iter.next_item().await {
-        if live_id.as_str() == "" {
-            if let Ok(sub) = sub_str.parse::<Subscription>() {
-                subs.push(sub);
-            } else {
-                log::error!("Database error: invalid sub string: {}", sub_str);
-            };
+    let mut iter: AsyncIter<(Subscription, String)> = db_clone.hscan_match("subs", format!("{platform}:*")).await.unwrap();
+    while let Some((sub, live_id)) = iter.next_item().await {
+        if live_id.is_empty() {
+            subs.push(sub);
         } else {
             if let Some(live) = api.live_status(&live_id, None).await {
                 let mut db_clone = db.clone();
-                let mut iter: AsyncIter<(String, i32)> = db_clone.hscan(&sub_str).await.unwrap();
+                let mut iter: AsyncIter<(String, i32)> = db_clone.hscan(&sub).await.unwrap();
                 match live.get_state() {
                     LiveState::Running => (),
                     LiveState::Ended | LiveState::TimedOut => {
@@ -33,8 +29,8 @@ pub async fn check<T: Metadata + Display>(api: Arc<impl API<T>>, db: &mut Multip
                                     show_above_text: Default::default()
                                 }).reply_parameters(ReplyParameters::new(MessageId(msg_id))).await.unwrap();
                             redis::pipe().atomic()
-                                .hset("subs", &sub_str, "")
-                                .hset(&sub_str, chat_id, 0)
+                                .hset("subs", &sub, "")
+                                .hset(&sub, chat_id, 0)
                                 .exec_async(db).await.unwrap();
                         }
                     }
@@ -49,7 +45,7 @@ pub async fn check<T: Metadata + Display>(api: Arc<impl API<T>>, db: &mut Multip
     }
     for live in api.user_live_status(subs).await {
         let sub = live.to_sub();
-        let subscribers: Vec<String> = db.hkeys(sub.to_db_string()).await.unwrap();
+        let subscribers: Vec<String> = db.hkeys(&sub).await.unwrap();
         for chat_id in subscribers {
             let msg = bot.send_message(chat_id.clone(), live.to_string()).link_preview_options(LinkPreviewOptions {
                 is_disabled: true,
@@ -59,8 +55,8 @@ pub async fn check<T: Metadata + Display>(api: Arc<impl API<T>>, db: &mut Multip
                 show_above_text: Default::default()
             }).await.unwrap();
             redis::pipe().atomic()
-                .hset("subs", sub.to_db_string(), live.get_id())
-                .hset(sub.to_db_string(), chat_id, msg.id.0)
+                .hset("subs", &sub, live.get_id())
+                .hset(&sub, chat_id, msg.id.0)
                 .exec_async(db).await.unwrap();
         }
     }
