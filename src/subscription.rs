413f1f6d9bc14ce954747fa6a009dcc816330bc3
Mike Lei <mikelei8291@users.noreply.github.com>
2025-04-27 19:39:23 +0800

    refactor(subscription): simplify error handling and array destructuring

--- src/subscription.rs
+++ src/subscription.rs
@@ -35,12 +35,9 @@ impl ToRedisArgs for Subscription {
 
 impl FromRedisValue for Subscription {
     fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
-        String::from_redis_value(v)
-            .map(|s| s.parse().map_err(
-                |e: <Subscription as FromStr>::Err| RedisError::from(
-                    (ErrorKind::TypeError, "Invalid database value", e.to_string())
-                )
-            ))?
+        String::from_redis_value(v)?
+            .parse::<Subscription>()
+            .map_err(|e| RedisError::from((ErrorKind::TypeError, "Invalid database value", e.to_string())))
     }
 }
 
@@ -54,15 +51,12 @@ impl FromStr for Subscription {
     type Err = SubscriptionError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let split: Vec<&str> = s.split(":").collect();
-        let (Some(&platform_str), Some(&id), Some(&username)) = (split.get(0), split.get(1), split.get(2)) else {
+        let split: Vec<_> = s.split(":").collect();
+        let [platform_str, id, username] = split[..] else {
             return Err(SubscriptionError::InvalidFormat);
         };
-        let Ok(platform) = Platform::from_str(platform_str) else {
-            return Err(SubscriptionError::UnsupportedPlatform);
-        };
-        let user = User { id: id.to_owned(), username: username.to_owned() };
-        Ok(Subscription { platform, user })
+        let platform = platform_str.parse().or(Err(SubscriptionError::UnsupportedPlatform))?;
+        Ok(Subscription { platform, user: User { id: id.to_owned(), username: username.to_owned() } })
     }
 }
 
@@ -70,30 +64,18 @@ impl Subscription {
     const URL_REGEX: Lazy<Regex> = lazy_regex!(r"^https?://.+$");
 
     async fn from_host_and_path(host: &str, path: &str) -> Result<Self, ParseError> {
-        if let Ok(platform) = host.parse::<Platform>() {
-            if let Some(user) = platform.parse_user(path).await {
-                return Ok(Subscription { platform, user });
-            } else {
-                return Err(ParseError::IncorrectFormat("Invalid username".into()));
-            }
-        } else {
-            return Err(ParseError::Custom(format!("Unsupported platform: {host}").into()));
-        };
+        let platform: Platform = host.parse().or(Err(ParseError::Custom(format!("Unsupported platform: {host}").into())))?;
+        let user = platform.parse_user(path).await.ok_or(ParseError::IncorrectFormat("Invalid username".into()))?;
+        Ok(Self { platform, user })
     }
 
     pub async fn from_url(mut input: String) -> Result<Self, ParseError> {
         if !Self::URL_REGEX.is_match(&input) {
             input = format!("https://{input}");
         }
-        match input.parse::<Url>() {
-            Ok(url) => {
-                match url.host() {
-                    Some(host) => Ok(Self::from_host_and_path(host.to_string().as_str(), url.path()).await?),
-                    None => Err(ParseError::IncorrectFormat("Hostname not found".into()))
-                }
-            }
-            Err(err) => Err(ParseError::IncorrectFormat(err.into()))
-        }
+        let url = input.parse::<Url>().map_err(|err| ParseError::IncorrectFormat(err.into()))?;
+        let host = url.host_str().ok_or(ParseError::IncorrectFormat("Hostname not found".into()))?;
+        Self::from_host_and_path(host, url.path()).await
     }
 
     pub fn to_db_string(&self) -> String {
